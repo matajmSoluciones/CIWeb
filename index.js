343125a97ce3c5e9c5e33d3a17dfe3c9b33336d1
Jhonny Mata <solucionesmatajm@gmail.com>
2017-04-06 12:53:17 -0400

    correcciones tbrain node

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Matrix=require("./src/matrix.js");
 var MFunction=require("./src/mfunction.js");
+var Tbrain=require("./src/tbrain.js");
 module.exports={
 	Matrix:Matrix,
 	MFunction:MFunction
+	Tbrain:Tbrain
 }
